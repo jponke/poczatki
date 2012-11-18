9516e72a68e8ea593dc213ae30836e525a78bbad
Jakub Ponke <kubus.gda@gmail.com>
2012-11-18 12:02:39 +0100

    gwiazdki

---
+++ poczatki/cw1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+  int q=0, z=0;
+ 
+  for (int a = 0; a<3; a++ ){
+  while(z<=3)
+{
+    printf("* ");   
+    z=z+1;   
+}
+ printf("\n");
+
+  while(q<=3)
+{
+   printf(" *");
+   q=q+1;
+    
+}
+  }
+  printf("\n");
+}
