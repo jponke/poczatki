ba0bc20c10a3cc531ff64b402db3cab8d7795218
Jakub Ponke <kubus.gda@gmail.com>
2012-11-18 12:30:29 +0100

    wypelnianie i wyswietalnie zawartosci tablicy

---
+++ poczatki/cw3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int tab[5];
+int main(){
+  printf("podaj 6 liczb: \n");
+  for (int i=0; i<6; i++){  
+scanf("%d",&tab[i]);
+  }
+  for (int i=0; i<6; i++){
+  printf("%d\n",tab[i]);
+  }
+
+}
