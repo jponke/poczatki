d1f5c04aa3ef0687f4e081bf3b06b91e0089567c
Jakub Ponke <kubus.gda@gmail.com>
2012-11-18 12:13:00 +0100

    poprawki

--- poczatki/cw1.c
+++ poczatki/cw1.c
@@ -4,19 +4,22 @@ int main(){
   int q=0, z=0;
  
   for (int a = 0; a<3; a++ ){
+    z=0;
   while(z<=3)
 {
     printf("* ");   
-    z=z+1;   
+    z=z+1;  
+ 
 }
- printf("\n");
-
+printf("\n"); 
+ q=0;
   while(q<=3)
 {
    printf(" *");
    q=q+1;
-    
-}
-  }
-  printf("\n");
+ 
+}  
+   printf("\n"); 
+ }
+
 }
