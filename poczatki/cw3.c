73b03d92738da95abce67a7e38072072f99ba697
Jakub Ponke <kubus.gda@gmail.com>
2012-11-18 12:50:27 +0100

    sprawdza najwieksza liczbe z tabeli

--- poczatki/cw3.c
+++ poczatki/cw3.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
 int tab[5];
+int x;
 int main(){
   printf("podaj 6 liczb: \n");
   for (int i=0; i<6; i++){  
 scanf("%d",&tab[i]);
   }
+  printf("\n");
   for (int i=0; i<6; i++){
   printf("%d\n",tab[i]);
   }
+  printf("\n\n");
+  x=tab[0];
 
+  for(int i=1; i<=5; i++){
+    if(x<tab[i]){
+      x=tab[i];
+      
+    }
+}
+  printf("najwieksza jest liczby: %d\n",x);
 }
