f7f81d4ba31dfda7d2597e8db34057f2e1222d5b
Jakub Ponke <kubus.gda@gmail.com>
2012-11-25 09:36:04 +0100

    ostateczna wersja

---
+++ poczatki/cw5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int tab[5];
+int x;
+int main(){
+  printf("podaj 6 liczb: \n");
+  for (int i=0; i<6; i++){  
+scanf("%d",&tab[i]);
+  }
+  printf("\n");
+  for (int i=0; i<6; i++){
+  printf("%d\n",tab[i]);
+  }
+  printf("\n\n");
+  x=tab[0];
+
+  for(int i=1; i<=5; i++){
+    if(x<tab[i]){
+      x=tab[i];
+      
+    }
+}
+  printf("najwieksza jest liczby: %d\n",x);
+  for(int i=0; i<=5; i++){
+    if(tab[i]>0){
+      printf("liczba dodatnia z tab: %d\n",tab[i]);
+    }
+  }
+}
